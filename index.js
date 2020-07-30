d81668b5c6a0e8ddeb8dd15018ddcdb71bd9816a
Reid Garner <reidbgarner@gmail.com>
2020-07-30 10:10:31 -0600

    trying to fix pdf centering.

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ const styles = StyleSheet.create({
     progressContainer: {
         flex: 1,
         justifyContent: 'center',
-        // alignItems: 'center'
+        alignItems: 'flex-start',
     },
     progressBar: {
         width: 200,
